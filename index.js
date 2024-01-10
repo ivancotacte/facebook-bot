62a93a545af752f97ab8146c8acc8585f49f388e
ivancotacte <cotactearmenion@gmail.com>
2024-01-10 22:50:09 +0800

    Update: Files

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const login = require("fca-project-orion");
+
+login(
+  { appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")) },
+  (err, api) => {
+    if (err) return console.error(err);
+
+    api.setOptions({
+      logLevel: "silent",
+      forceLogin: true,
+      listenEvents: true,
+      autoMarkDelivery: false,
+      selfListen: true,
+    });
+
+    var stopListening = api.listenMqtt((err, event) => {
+      if (err) return console.error(err);
+
+      switch (event.type) {
+        case "message":
+            const input = event.body.toLowerCase();
+            if (input.startsWith("ping")) {
+                api.sendMessage("Pong!", event.threadID);
+            }
+        case "event":
+          console.log(event);
+          break;
+      }
+    });
+  }
+);
